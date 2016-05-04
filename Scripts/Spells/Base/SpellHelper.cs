83cf58bf563832a83247ea3e9bba27f92779b69d
Scott L <salawrence0@gmail.com>
2016-05-04 11:27:50 -0600

    Allow mark/recall/tele everywhere

--- Scripts/Spells/Base/SpellHelper.cs
+++ Scripts/Spells/Base/SpellHelper.cs
@@ -621,13 +621,13 @@ namespace Server.Spells
         private static readonly bool[,] m_Rules = new bool[,]
         {
 					/*T2A(Fel),	Khaldun,	Ilshenar,	Wind(Tram),	Wind(Fel),	Dungeons(Fel),	Solen(Tram),	Solen(Fel),	CrystalCave(Malas),	Gauntlet(Malas),	Gauntlet(Ferry),	SafeZone,	Stronghold,	ChampionSpawn,	Dungeons(Tokuno[Malas]),	LampRoom(Doom),	GuardianRoom(Doom),	Heartwood,	MLDungeons */
-/* Recall From */	{ false,	false,		true,		true,		false,		false,			true,			false,		false,				false,				false,				true,		true,		false,			true,						false,			false,				false,		false },
-/* Recall To */		{ false,	false,		false,		false,		false,		false,			false,			false,		false,				false,				false,				false,		false,		false,			false,						false,			false,				false,		false },
-/* Gate From */		{ false,	false,		false,		false,		false,		false,			false,			false,		false,				false,				false,				false,		false,		false,			false,						false,			false,				false,		false },
-/* Gate To */		{ false,	false,		false,		false,		false,		false,			false,			false,		false,				false,				false,				false,		false,		false,			false,						false,			false,				false,		false },
-/* Mark In */		{ false,	false,		false,		false,		false,		false,			false,			false,		false,				false,				false,				false,		false,		false,			false,						false,			false,				false,		false },
-/* Tele From */		{ true,		true,		true,		true,		true,		true,			true,			true,		false,				true,				true,				true,		false,		true,			true,						true,			true,				false,		true },
-/* Tele To */		{ true,		true,		true,		true,		true,		true,			true,			true,		false,				true,				false,				false,		false, 		true,			true,						true,			true,				false,		false },
+/* Recall From */	{ true,	true,		true,		true,		true,		true,			true,			true,		true,				true,				true,				true,		true,		true,			true,						true,			true,				true,		true },
+/* Recall To */		{ true,	true,		true,		true,		true,		true,			true,			true,		true,				true,				true,				true,		true,		true,			true,						true,			true,				true,		true },
+/* Gate From */		{ true,	true,		true,		true,		true,		true,			true,			true,		true,				true,				true,				true,		true,		true,			true,						true,			true,				true,		true },
+/* Gate To */		{ true,	true,		true,		true,		true,		true,			true,			true,		true,				true,				true,				true,		true,		true,			true,						true,			true,				true,		true },
+/* Mark In */		{ true,	true,		true,		true,		true,		true,			true,			true,		true,				true,				true,				true,		true,		true,			true,						true,			true,				true,		true },
+/* Tele From */		{ true,		true,		true,		true,		true,		true,			true,			true,		true,				true,				true,				true,		true,		true,			true,						true,			true,				true,		true },
+/* Tele To */		{ true,		true,		true,		true,		true,		true,			true,			true,		true,				true,				true,				true,		true, 		true,			true,						true,			true,				true,		true },
         };
 
         public static void SendInvalidMessage(Mobile caster, TravelCheckType type)
